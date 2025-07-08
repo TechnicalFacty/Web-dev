a2aed70b0fc34f7ac923327bc0c0dd4d021d68f6
TechnicalFacty <sweetaman768@gmail.com>
2025-07-08 12:35:36 +0530

    24 js operators added

---
+++ 24_oprators/index.js
@@ -0,0 +1,54 @@
+// Assignment Operator
+
+let x = 10;
+// Assign value 10 to x
+
+
+// Addition Operator
+let a = 10;
+let b = 2;
+let c = a + b;
+
+
+// Multiplication Operator
+let d = a * b;
+
+
+// Arithmetic Operators
+
+// + Addition
+// - Subtration
+// * Multiplication
+// ** Exponentiation
+// / Division
+// % Modulus (division Remainder)
+// ++ Increment  => a++, value of a will increase by 1
+//  -- Decrement => a--, value of a will decrease by 1
+
+// Assignment Operators
+
+// =, x=y, x=y
+// +=, x+=y, x = x+y
+// -+, x-=y, x = x-y
+// *=, x*=y, x = x*y
+// /=, x/=y, x = x/y
+// %=, x%=y, 
+// **=, x**=y,
+
+// Comparision Operators
+
+// == equal to
+// === equal value and equal type
+// != not equal
+// !== not equal or not equal type
+// > greater than
+// < less than
+// >= greater than or equal to 
+//  >= less than or equal to
+// ? ternary operator
+
+// Logical Operators
+
+// && logical and
+// || logical or
+// ! logical not
