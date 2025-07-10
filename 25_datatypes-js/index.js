9a3e0acc0b2dc873319c3adbc2c215ebca2004ba
TechnicalFacty <sweetaman768@gmail.com>
2025-07-10 13:38:22 +0530

    25_datatypes-Js

---
+++ 25_datatypes-js/index.js
@@ -0,0 +1,57 @@
+// Numbers
+let x2 = 6;
+let y1=6.7;
+
+// string
+let color = "red"
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object
+const person = {
+    firstName:  "John",
+    lastName:"Doe"
+}
+
+// array object
+const cars = ["Volvo","BMW"]
+
+
+//       20 + "string"+"16"+"4"
+let a = 16+4+"BMW"+16+4;
+console.log(a)
+
+
+let b; //b is undefined
+b=1;//b is number
+b ="John Doe"//now b is string
+
+
+b ="John's book is math"
+let c = '"john" is in class 7th'
+
+
+
+
+
+let d = BigInt("1231534713712490894716472164785491")
+
+
+
+
+// boolean
+let e = 5
+let f = 5
+let g = 6
+(e==f)//true
+(e==g)//false
+
+
+
+typeof "" //string
+
+typeof 0//number
+
+let h = "" 
