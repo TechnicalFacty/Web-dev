1f9eb86992173dcf0d21ff3f8a56461320478a01
TechnicalFacty <sweetaman768@gmail.com>
2025-07-15 21:43:05 +0530

    condition statements added

---
+++ 27_if-else-statements/index.js
@@ -0,0 +1,30 @@
+// If esle Statement
+let num = 54
+
+if (num>=18){
+    console.log("You can vote")
+}
+else{
+    console.log("You cannot vote")
+}
+
+//
+let number = 0
+
+if(number>0){
+    console.log("Positive Number")
+}
+else if(number<0){
+    console.log("Negative Number")
+}
+else {
+    console.log("Number is 0")
+}
+
+
+
+
+let  loggedin = true
+if(loggedin){
+    console.log("Welcome User")
+}
